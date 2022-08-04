09e1fe33594de7cf7181929632bb44ccf395ebc2
Nico Bellante <nico.bellante@discordapp.com>
2022-08-04 18:40:56 +0000

    changing some lsp stuff and a mapping

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -4,7 +4,7 @@ M.telescope = {
   n = {
     ["<leader><leader>"] = { "<cmd> Telescope<CR>", "open telescope" },
     ["<leader><leader>o"] = { "<cmd> Telescope oldfiles<CR>", "open telescop oldfiles" },
-    ["\\\\"] = { "<cmd> Telescope find_files<CR>", "file finder" },
+    ["\\\\"] = { "<cmd> Telescope oldfiles<CR>", "old files" },
     ["\\f"] = { "<cmd> Telescope live_grep<CR>", "open telescope live grep" },
   },
 }
@@ -14,6 +14,7 @@ M.shortcuts = {
     ["<leader>m"] = { "<cmd> :e ~/.config/nvim/lua/custom/mappings.lua <CR>", "edit mappings" },
     ["<leader>v"] = { "<cmd> :e ~/.config/nvim/lua/custom/chadrc.lua <CR>", "edit nvim mappings" },
     ["<leader>p"] = { "<cmd> :e ~/.config/nvim/lua/custom/plugins/init.lua <CR>", "edit nvim plugins" },
+    ["<leader>cm"] = { "<cmd> :vsplit %<.module.css <CR>", "open corresponding css module" },
   },
 }
 
--- custom/plugins/lspconfig.lua
+++ custom/plugins/lspconfig.lua
@@ -1,14 +1,20 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "emmet_ls", "clangd", "pyright", "tsserver" }
+local lspconfig = require("lspconfig")
+local servers = { "clangd", "pyright", "tsserver" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  lspconfig[lsp].setup({
     on_attach = on_attach,
     capabilities = capabilities,
     root_dir = vim.loop.cwd,
-  }
+  })
 end
 
+lspconfig.cssmodules_ls.setup({
+  on_attach = function(client)
+    client.resolved_capabilities.goto_definition = false
+    on_attach(client)
+  end,
+})
